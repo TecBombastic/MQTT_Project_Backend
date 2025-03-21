23812a7c20a438c2f690a2bee495525ab094e57f
gaelosglez <151054581+gaelosglez@users.noreply.github.com>
2025-03-21 01:10:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { router as pedidos } from './routes/pedidos.js'
+
 const app = express()
 const PORT = 3000
+
 dotenv.config() // Carga las variables de entorno
 app.use(express.json()) // Middleware para parsear JSON
-app.use('/', pedidos) // Es dice que la ruta de pedidos estarán accesibles desde la raíz /
+
+app.use('/', pedidos) // Es dice que la ruta de pedidos estarán accesibles desde la raíz 
+
 app.get('/', (req, res) => {
-    res.send('Hola desde el API')
+    res.send('Tienda API')
 })
+
 app.listen(PORT, () => { // Inicia el servidor
     console.log(`Servidor inciado en el puerto ${PORT}`)
 })
